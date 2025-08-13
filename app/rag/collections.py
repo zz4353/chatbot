35b69b67da55a6b94267c03baf7b1d98d07c151b
zz4353 <vmkqa2@gmail.com>
2025-08-13 09:51:55 +0700

    .

--- app/rag/collections.py
+++ app/rag/collections.py
@@ -2,8 +2,8 @@ from app.db.qdrant import VectorStore
 from app.embedding.models import DenseEmbedding, SparseEmbedding
 
 dense_model = DenseEmbedding()
-sparse_model = SparseEmbedding()
+sparse_model1 = SparseEmbedding()  # mỗi collection tạo 1 sparse model riêng. 
 
 COLLECTIONS = {
-    "stock": VectorStore(collection_name="stock_collection", dense_model=dense_model, sparse_model=sparse_model),
+    "stock": VectorStore(collection_name="stock_collection", dense_model=dense_model, sparse_model=sparse_model1),
 }
